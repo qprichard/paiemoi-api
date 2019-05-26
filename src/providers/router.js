623e8cf8c13b55cb04fbc88bd756f1e967712ae2
quentin <quentinrichard97@gmail.com>
2019-05-26 15:49:51 +0200

    :add router provider

--- src/providers/router.js
+++ src/providers/router.js
@@ -6,14 +6,14 @@ const ControllerProvider = require('./controllerProvider');
  * @param {Class} Model - the Model linked to the route
  * @return {array} - Array of routes to use with fastify
 */
-const routeProvider = (name, _Model) => {
-  const _controller = new ControllerProvider(_Model);
+const router = (name, _Model, _Controller = ControllerProvider) => {
+  const _controller = new _Controller(_Model);
 
   return [
     {
       method: 'GET',
       url: `/api/${name}`,
-      handler: () => _controller.all()
+      handler: (...args) => _controller.all(...args)
     },
     {
       method: 'POST',
@@ -53,4 +53,4 @@ const routeProvider = (name, _Model) => {
   ]
 }
 
-module.exports = routeProvider;
+module.exports = router;
