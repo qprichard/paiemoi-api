52e7a9f6f6a2cbaad7ed2c5bcd9164bb4a5a15a1
Quentin RICHARD <quentin.richard@weezevent.com>
2019-05-30 20:44:45 +0200

    add toAPI function

---
+++ src/__tests__/helpers/toApi.js
@@ -0,0 +1,81 @@
+const toAPI = require('../../helpers/toApi');
+const expect = require('chai').expect;
+
+describe('Test helper toAPI', () => {
+  class Test {
+    constructor({
+      param1,
+      param2,
+    }) {
+      this.param1 = param1
+      this.param2 = param2
+    }
+  }
+
+  class TestAPI{
+    constructor({
+      param1,
+      param2
+    }){
+      this.param1 = param1;
+      this.param2 = param2;
+    }
+
+    toAPI() {
+      return {
+        param1: this.param1
+      }
+    }
+  }
+
+  it('should retrieve the incoming single data', () => {
+    const myData = new Test({ param1: 'test', param2: 'test' });
+
+    expect(toAPI(myData)).to.deep.equal({
+      param1: 'test',
+      param2: 'test'
+    });
+  });
+
+  it('should retrieve formated single data', () => {
+    const myData = new TestAPI({ param1: 'test', param2: 'test' });
+
+    expect(toAPI(myData)).to.deep.equal({
+      param1: 'test',
+    });
+  });
+
+  it('should retrieve multiple incoming datas', () => {
+    const myDatas = [
+      new Test({ param1: 'test-1', param2: 'test-1'}),
+      new Test({ param1: 'test-2', param2: 'test-2'}),
+    ]
+
+    expect(toAPI(myDatas)).to.deep.equals([
+      {
+        param1: 'test-1',
+        param2: 'test-1',
+      },
+      {
+        param1: 'test-2',
+        param2: 'test-2',
+      },
+    ])
+  });
+
+  it('should retrieve multiple formated datas', () => {
+    const myDatas = [
+      new TestAPI({ param1: 'test-1', param2: 'test-1'}),
+      new TestAPI({ param1: 'test-2', param2: 'test-2'}),
+    ]
+
+    expect(toAPI(myDatas)).to.deep.equals([
+      {
+        param1: 'test-1',
+      },
+      {
+        param1: 'test-2',
+      },
+    ])
+  });
+});
